7c56cefb4fc2f9567894003637bdc03ea01c5bff
Thomas Chille <thomas@chille.de>
2025-01-19 20:57:53 +0700

    updated flux deps

--- pix2pix/setup.py
+++ pix2pix/setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="flux",
     version="0.0.1",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=find_packages(),
     install_requires=[
         "torch",
         "torchvision",
