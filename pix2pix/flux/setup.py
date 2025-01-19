a3a7cba774f2373fe254182e52e0fb32512f8d1e
Thomas Chille <thomas@chille.de>
2025-01-19 21:13:57 +0700

    moved setup.py to correct loction

--- pix2pix/flux/setup.py
+++ pix2pix/flux/setup.py
@@ -1,3 +1,17 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup()
+setup(
+    name="flux",
+    version="0.0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "torch",
+        "torchvision",
+        "transformers",
+        "einops",
+        "fire",
+        "invisible-watermark",
+        "safetensors"
+    ]
+)
